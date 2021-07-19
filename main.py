4ad462887942f06a31a02269ce014f085f76a666
mirantha <mj.jayathilaka@gmail.com>
2021-07-19 14:45:22 +0530

    add files

---
+++ main.py
@@ -0,0 +1,159 @@
+import streamlit as st
+from PIL import Image
+import subprocess
+from utils.generate_yaml import *
+import pandas as pd
+
+# import time
+# with st.empty():
+#     for seconds in range(60):
+#         st.write(f"⏳ {seconds} seconds have passed")
+#         time.sleep(1)
+#     st.write("✔️ 1 minute over!")
+fav = Image.open('images/fav.png')
+st.beta_set_page_config(
+    page_title='kudle', page_icon=fav, initial_sidebar_state='auto')
+# st.title('kudle v0.1')
+image = Image.open('images/logo1.png')
+st.image(image, width=200)
+
+
+def run_command_login(args):
+    # st.info(f"Running '{' '.join(args)}'")
+    output = subprocess.getoutput(args)
+    # st.info(output)
+    with placeholder3.beta_container():
+        st.write(str(output))
+
+
+def run_command_getip(args):
+    # st.info(f"Running '{' '.join(args)}'")
+    output = subprocess.getoutput(args)
+    # st.info(output)
+    output_mod = output.split(' ')
+    # index_ = output_mod.index('AGE\napi-app')
+    # df = pd.DataFrame(columns=['App Name', 'Public IP'])
+    # df.loc[0] = [output_mod[36].split('\n')[1]] + [output_mod[48]]
+    with placeholder4.beta_container():
+        # st.write(str(output_mod))
+        st.write(output)
+
+
+def run_command(args):
+    # st.info(f"Running '{' '.join(args)}'")
+    output = subprocess.getoutput(args)
+    # st.info(output)
+    with placeholder2.beta_container():
+        st.write(str(output))
+
+
+if st.button('Login to your azure account'):
+    placeholder3 = st.empty()
+    run_command_login('az login')
+
+
+# check for running nodes
+# run_command('az login')
+
+with st.beta_expander("New Deployment"):
+    st.markdown('## New Kubernetes Deployment')
+
+    # repo_link = st.text_input('GitHub repo link of your project', '')
+    docker_image = st.text_input('Your docker image name', '')
+    dep_name = st.text_input('Deployment name', '')
+    version = st.text_input('Version', '')
+
+    st.markdown('### Azure Configuration Inputs')
+
+    resourcegroup = st.text_input('Resource group name', '')
+    resourcegroup_createnew = st.checkbox(
+        'Create new', key='resourcegroup_createnew')
+
+    acrname = st.text_input('Container registry name', '')
+    acr_createnew = st.checkbox('Create new', key='acr_createnew')
+
+    aksname = st.text_input('Kubernetes cluster name', '')
+    aks_createnew = st.checkbox('Create new', key='aks_createnew')
+
+    if st.button('Deploy'):
+        my_bar = st.progress(0)
+        placeholder = st.empty()
+        placeholder2 = st.empty()
+
+        if resourcegroup_createnew:
+            with placeholder.beta_container():
+                st.write('Creating Resource Group')
+            run_command('az group create --name ' +
+                        resourcegroup + ' --location eastus')
+            with placeholder.beta_container():
+                st.write('Resource Group created')
+        my_bar.progress(10)
+
+        # ACR creation and login
+        if acr_createnew:
+            with placeholder.beta_container():
+                st.write('Creating Container Registry')
+            run_command(
+                'az acr create --resource-group ' + resourcegroup + ' --name ' + acrname + ' --sku Basic')
+            with placeholder.beta_container():
+                st.write('Container Registry created')
+        run_command('az acr login --name ' + acrname)
+        with placeholder.beta_container():
+            st.write('Successfully logged into Container Registry')
+        my_bar.progress(20)
+
+        # Docker image
+        with placeholder.beta_container():
+            st.write('Tagging docker image')
+        run_command('docker tag ' + docker_image + ' ' +
+                    acrname + '.azurecr.io/'+docker_image+':' + version)
+        with placeholder.beta_container():
+            st.write('Docker image tagged')
+
+        my_bar.progress(40)
+        with placeholder.beta_container():
+            st.write('Pushing docker image to Container Registry')
+        run_command('docker push '+acrname+'.azurecr.io/' +
+                    docker_image+':' + version)
+        with placeholder.beta_container():
+            st.write('Docker image pushed to Container Registry')
+        my_bar.progress(50)
+        # AKS creation and login
+        if aks_createnew:
+            with placeholder.beta_container():
+                st.write('Creating Kubernetes Service')
+            run_command('az aks create' +
+                        ' --resource-group ' + resourcegroup +
+                        ' --name ' + aksname +
+                        ' --node-count 2' +
+                        ' --generate-ssh-keys' +
+                        ' --attach-acr ' + acrname)
+        my_bar.progress(60)
+        with placeholder.beta_container():
+            st.write('Kubernetes service created')
+
+        run_command(
+            'az aks get-credentials --resource-group ' + resourcegroup+' --name ' + aksname)
+        my_bar.progress(70)
+        with placeholder.beta_container():
+            st.write('Logged into Kubernetes Service')
+
+        # Generate the yaml file
+        my_bar.progress(80)
+        generate_yaml(dep_name, acrname, version, docker_image)
+        with placeholder.beta_container():
+            st.write('Kubernetes configuration file generated')
+
+        with placeholder.beta_container():
+            st.write('Deploying Kubernetes...')
+        run_command('kubectl apply -f utils/' + dep_name + '.yaml')
+        my_bar.progress(90)
+        with placeholder.beta_container():
+            st.write('Kubernetes deployed')
+        my_bar.progress(100)
+
+with st.beta_expander("Check deployment status"):
+    # dep_name2 = st.text_input('Deployment name', '')
+    if st.button('Get public IP'):
+        placeholder4 = st.empty()
+        run_command_getip('kubectl get service '+dep_name)
---
+++ utils/generate_yaml.py
@@ -0,0 +1,24 @@
+import yaml
+
+
+def generate_yaml(dep_name, acrname, version, docker_image):
+    with open('dep-name.yaml', "r") as stream:
+        docs = yaml.load_all(stream)
+        dict_docs = []
+        for doc in docs:
+            dict_docs.append(doc)
+
+    dict_docs[0]['metadata']['name'] = dep_name
+    dict_docs[0]['spec']['selector']['matchLabels']['app'] = dep_name
+    dict_docs[0]['spec']['template']['metadata']['labels']['app'] = dep_name
+    dict_docs[0]['spec']['selector']['matchLabels']['app'] = dep_name
+    dict_docs[0]['spec']['template']['spec']['containers'][0]['name'] = dep_name
+    dict_docs[0]['spec']['template']['spec']['containers'][0]['image'] = acrname + \
+        '.azurecr.io/'+docker_image+':' + version
+    dict_docs[1]['metadata']['name'] = dep_name
+    dict_docs[1]['spec']['selector']['app'] = dep_name
+
+    with open(dep_name+'.yaml', 'a') as outfile:
+        yaml.dump(dict_docs[0], outfile, default_flow_style=False)
+        outfile.write('--- \n')
+        yaml.dump(dict_docs[1], outfile, default_flow_style=False)
